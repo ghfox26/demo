4deeef8e059c82927b1be9be698bce617f901c88
ghfox26 <fox26@126.com>
2019-01-03 17:03:15 +0800

    增加本地仓库

--- demo/src/main/java/com/example/demo/MyController.java
+++ demo/src/main/java/com/example/demo/MyController.java
@@ -13,46 +13,49 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 public class MyController {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(MyController.class);
-	
-	@Autowired 
+
+	@Autowired
 	private TestRepository testRepository;
-	
+
 	@RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!"; 
-    }
-	
+	public String index() {
+		return "Greetings from Spring Boot Test!\n" + 
+	           "number?thisNumber=11000\n" + 
+			   "number1?thisNumber=11000\n"  +
+	           "number2?thisNumber=11000\n"+
+	           "number3?thisNumber=11000\n";
+	}
+
 	@RequestMapping("/test")
-    public @ResponseBody Iterable<TestRecord> test() {
-		int i=100000;
+	public @ResponseBody Iterable<TestRecord> test() {
+		int i = 100000;
 		log.info(this.electNumber3(i).toString());
 		log.info(this.electNumber2(i).toString());
 		log.info(this.electNumber1(i).toString());
 		log.info(this.electNumber(i).toString());
-		
-		i=1000000;
+
+		i = 1000000;
 		log.info(this.electNumber3(i).toString());
 		log.info(this.electNumber2(i).toString());
 		log.info(this.electNumber1(i).toString());
 		log.info(this.electNumber(i).toString());
-		i=10000000;
+		i = 10000000;
 		log.info(this.electNumber3(i).toString());
-		log.info(this.electNumber2(i).toString());
-		//log.info(this.electNumber1(i).toString());
-		//log.info(this.electNumber(i).toString());
-		i=100000000;
-		log.info(this.electNumber3(i).toString());
-		//log.info(this.electNumber2(i).toString());
-		//log.info(this.electNumber1(i).toString());
-		//log.info(this.electNumber(i).toString());
-        return testRepository.findAll(); 
-    }
+		// log.info(this.electNumber2(i).toString());
+		// log.info(this.electNumber1(i).toString());
+		// log.info(this.electNumber(i).toString());
+		i = 100000000;
+		// log.info(this.electNumber3(i).toString());
+		// log.info(this.electNumber2(i).toString());
+		// log.info(this.electNumber1(i).toString());
+		// log.info(this.electNumber(i).toString());
+		return testRepository.findAll();
+	}
+
 	@RequestMapping("/number")
 	public TestRecord electNumber(@RequestParam int thisNumber) {
 		Long timeStart = System.currentTimeMillis();
@@ -77,27 +80,28 @@ public class MyController {
 				list.removeAll(removeList);
 				log.info("第{}轮结束！结果如下：", b);
 				log.debug(list.toString());
-				removeList.clear();;
+				removeList.clear();
+				;
 				log.info("a={}", a);
 			} else {
 				break;
 			}
 		}
-		Long timeEnd = System.currentTimeMillis();  
-		TestRecord t=new TestRecord();
+		Long timeEnd = System.currentTimeMillis();
+		TestRecord t = new TestRecord();
 		t.setNumber(thisNumber);
 		t.setCounts(a);
 		t.setRounds(b);
 		t.setRusult_number(list.get(0));
-		t.setTimes(timeEnd-timeStart);
+		t.setTimes(timeEnd - timeStart);
 		t.setFuncName("electNumber");
 		testRepository.save(t);
 		return t;
 	}
-	
+
 	@RequestMapping("/number1")
 	public TestRecord electNumber1(@RequestParam int thisNumber) {
-		Long timeStart = System.currentTimeMillis();  
+		Long timeStart = System.currentTimeMillis();
 		List<Integer> list = new ArrayList<Integer>();
 
 		for (int i = 1; i <= thisNumber; i++) {
@@ -110,13 +114,13 @@ public class MyController {
 				b++;
 				log.info("————————————————第{}轮开始：list.size={}，a={}", b, list.size(), a);
 				log.debug(list.toString());
-				for (Iterator<Integer> iterator = list.iterator(); iterator.hasNext(); ) {
+				for (Iterator<Integer> iterator = list.iterator(); iterator.hasNext();) {
 					a++;
 					iterator.next();
 					if (a % 2 == 1) {
 						iterator.remove();
 					}
-		        }
+				}
 				log.info("第{}轮结束！结果如下：", b);
 				log.debug(list.toString());
 				log.info("a={}", a);
@@ -124,13 +128,13 @@ public class MyController {
 				break;
 			}
 		}
-		Long timeEnd = System.currentTimeMillis();  
-		TestRecord t=new TestRecord();
+		Long timeEnd = System.currentTimeMillis();
+		TestRecord t = new TestRecord();
 		t.setNumber(thisNumber);
 		t.setCounts(a);
 		t.setRounds(b);
 		t.setRusult_number(list.get(0));
-		t.setTimes(timeEnd-timeStart);
+		t.setTimes(timeEnd - timeStart);
 		t.setFuncName("electNumber1");
 		testRepository.save(t);
 		return t;
@@ -138,10 +142,10 @@ public class MyController {
 
 	@RequestMapping("/number2")
 	public TestRecord electNumber2(@RequestParam int thisNumber) {
-		Long timeStart = System.currentTimeMillis();  
+		Long timeStart = System.currentTimeMillis();
 		List<Integer> list = new LinkedList<Integer>();
 
-		log.info("start:{}",thisNumber);
+		log.info("start:{}", thisNumber);
 		for (int i = 1; i <= thisNumber; i++) {
 			list.add(i);
 		}
@@ -153,13 +157,13 @@ public class MyController {
 				b++;
 				log.info("————————————————第{}轮开始：list.size={}，a={}", b, list.size(), a);
 				log.debug(list.toString());
-				for (Iterator<Integer> iterator = list.iterator(); iterator.hasNext(); ) {
+				for (Iterator<Integer> iterator = list.iterator(); iterator.hasNext();) {
 					a++;
 					iterator.next();
 					if (a % 2 == 1) {
 						iterator.remove();
 					}
-		        }
+				}
 				log.info("第{}轮结束！结果如下：", b);
 				log.debug(list.toString());
 				log.info("a={}", a);
@@ -167,19 +171,18 @@ public class MyController {
 				break;
 			}
 		}
-		Long timeEnd = System.currentTimeMillis();  
-		TestRecord t=new TestRecord();
+		Long timeEnd = System.currentTimeMillis();
+		TestRecord t = new TestRecord();
 		t.setNumber(thisNumber);
 		t.setCounts(a);
 		t.setRounds(b);
 		t.setRusult_number(list.get(0));
-		t.setTimes(timeEnd-timeStart);
+		t.setTimes(timeEnd - timeStart);
 		t.setFuncName("electNumber2");
 		testRepository.save(t);
 		return t;
 	}
-	
-	
+
 	@RequestMapping("/number3")
 	public TestRecord electNumber3(@RequestParam int thisNumber) {
 		Long timeStart = System.currentTimeMillis();
@@ -194,7 +197,7 @@ public class MyController {
 			if (list.size() > 1) {
 				b++;
 				log.info("————————————————第{}轮开始：list.size={}，a={}", b, list.size(), a);
-				//log.info(list.toString());
+				// log.info(list.toString());
 				for (int i = 0; i < list.size(); i++) {
 					a++;
 					if (a % 2 == 0) {
@@ -204,20 +207,21 @@ public class MyController {
 				list.clear();
 				list.addAll(removeList);
 				log.info("第{}轮结束！结果如下：", b);
-				//log.info(list.toString());
-				removeList.clear();;
+				// log.info(list.toString());
+				removeList.clear();
+				;
 				log.info("a={}", a);
 			} else {
 				break;
 			}
 		}
-		Long timeEnd = System.currentTimeMillis();  
-		TestRecord t=new TestRecord();
+		Long timeEnd = System.currentTimeMillis();
+		TestRecord t = new TestRecord();
 		t.setNumber(thisNumber);
 		t.setCounts(a);
 		t.setRounds(b);
 		t.setRusult_number(list.get(0));
-		t.setTimes(timeEnd-timeStart);
+		t.setTimes(timeEnd - timeStart);
 		t.setFuncName("electNumber3");
 		testRepository.save(t);
 		return t;
